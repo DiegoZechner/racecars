e6bd69778e716953828d61e1613449de89284de2
Diego <you@example.com>
2025-03-06 10:20:21 +0100

    Video 4 committed

--- src/app/components/ui/cars/Card.tsx
+++ src/app/components/ui/cars/Card.tsx
@@ -1,13 +1,24 @@
+"use client";
 import { Car } from "@/app/libs/types";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = { car: Car };
 
 export default function Card({ car }: Props) {
+  const [isActive, setisActive] = useState(false);
+
   return (
-    <div className="bg-green-100 border p-4">
-      <h2>{car.name}</h2>
+    <div
+      className={`shadow p-4 hover:cursor-pointer ${
+        isActive ? "bg-amber-200" : "bg-amber-50"
+      }`}
+      onClick={() => {
+        setisActive(!isActive);
+      }}
+    >
+      <h2 className="font-bold text-3xl">{car.name}</h2>
       <div>{car.type}</div>
+      {isActive ? "active" : "inactive"}
     </div>
   );
 }
